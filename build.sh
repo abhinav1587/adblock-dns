d05f4c1c03b588c722196f6c7ffd983d2d81e6ca
Arthur Kay <kyle@hemsley.info>
2015-06-20 04:13:54 -0400

    Revert "Small fixes"
    This reverts commit 2e00a5a4b3e202d0d2f877762d80b29a2cefa77d.

--- build.sh
+++ build.sh
@@ -42,6 +42,6 @@ wget -P download http://hosts-file.net/psh.txt
 sed -i -r 's/((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])/'"$HOLE"'/g' ./download/*
 
 #build image
-docker build -t arthurkay/sagittarius-a .
+docker build -t 'arthurkay/sagittarius-a .'
 #clean up
 rm -rf ./download
