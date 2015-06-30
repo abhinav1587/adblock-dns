be2f96eb9338627bef58550d01f1dfde40e4837a
Arthur Kay <kyle@hemsley.info>
2015-06-30 01:38:08 -0400

    Forgot detached deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-sudo docker run --name=saga-dns -p 53:53/tcp -p 53:53/udp --cap-add=NET_ADMIN arthurkay/sagittarius-a
+sudo docker run -d --name=saga-dns -p 53:53/tcp -p 53:53/udp --cap-add=NET_ADMIN arthurkay/sagittarius-a
